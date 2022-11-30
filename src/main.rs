b6adce3855b6138758aa71acdfd8fc945a554c6d
zkmaestro <zkmaestro@protonmail.com>
2022-11-30 00:22:06 +0000

    added more log info

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ async fn main() -> Result<(), Error> {
     // iterate each member and get the last 24 hours of tweets, then determine the sentiment of each user
     // based on the content of their tweets
     let now: DateTime<Utc> = Utc::now();
-    let day = now.format("%Y-%m-%d").to_string();
+    let day = "2022-11-29";//now.format("%Y-%m-%d").to_string();
     let mut total_tweets = 0;
     let mut user_sentiment_map = HashMap::new();
     for member in list_members {
@@ -40,9 +40,21 @@ async fn main() -> Result<(), Error> {
         user_sentiment_map.insert(member.name, sentiment.to_string());
     }
     info!("On {}, there were {} tweets in total. Thats and average of {} per user.", day, total_tweets, total_tweets/list_members_count);
-    for (name, sentiment) in user_sentiment_map {
+    for (name, sentiment) in &user_sentiment_map {
         info!("{} has a sentiment of {}", name, sentiment);
     }
+    // get the average sentiment of the list
+    let mut total_sentiment = 0.0;
+    for sentiment in user_sentiment_map.values() {
+        total_sentiment += sentiment.parse::<f64>().unwrap();
+    }
+    let average_sentiment = total_sentiment / list_members_count as f64;
+    info!("The total sentiment of the list across {} users is {}. The average sentiment of the list is {}.", list_members_count, average_sentiment, total_sentiment); 
+    // get the median sentiment of the list
+    let mut sentiments:Vec<f64> = user_sentiment_map.values().map(|s| s.parse::<f64>().unwrap()).collect();
+    sentiments.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    let median_sentiment = sentiments[sentiments.len() / 2];
+    info!("The median sentiment of the list is {}", median_sentiment);
 
     /*
     let user_id = config.twitter_user_id;
--- src/sentiment.rs
+++ src/sentiment.rs
@@ -2,15 +2,31 @@ use vader_sentiment as vader;
 use crate::twitter::{Tweet};
 use log::{info};
 
-
+/**
+ * Get the sentiment of a specific tweets
+ * 
+ * # Arguments
+ * 
+ * * `test` - Text of a tweet
+ */
 pub fn get_sentiment(text: &str) -> f64 {
 		let analyzer = vader::SentimentIntensityAnalyzer::new();
 		let sentiment = analyzer.polarity_scores(text);
-		let compound: f64 = sentiment.get("compound").unwrap().to_owned();
+		let mut compound: f64 = sentiment.get("compound").unwrap().to_owned();
+		if compound.is_nan() {
+				compound = 0.0;
+		}
 		info!("Sentiment for {} is {}", text, compound);
 		compound
 }
 
+/**
+ * Get the sentiment of a list of tweets
+ * 
+ * # Arguments
+ * 
+ * * `tweets` - A list of tweets
+ */
 pub fn get_sentiment_for_tweets(tweets: Vec<Tweet>) -> f64 {
 		let mut total_sentiment = 0.0;
 		let total_tweets = tweets.len();
